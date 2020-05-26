6a7fa87d97b61542d41a28f8ab2a296c65d7f19b
Dametrious Peyton <dametrious.peyton@sunbirddcim.com>
2020-05-26 17:44:15 -0400

    Update for SFOS 33
    Reorganized the project to be inline with the last QT project I did, harbour-nemosyne
    Tested on device and vm to make sure it still is functional

---
+++ fallingblocks/fallingblocks.pro
@@ -0,0 +1,83 @@
+# NOTICE:
+#
+# Application name defined in TARGET has a corresponding QML filename.
+# If name defined in TARGET is changed, the following needs to be done
+# to match new name:
+#   - corresponding QML filename must be changed
+#   - desktop icon filename must be changed
+#   - desktop filename must be changed
+#   - icon definition filename in desktop file must be changed
+#   - translation filenames have to be changed
+
+# The name of your application
+TARGET = harbour-fallingblocks
+
+CONFIG += sailfishapp
+#CONFIG += c++11
+#QMAKE_CXXFLAGS += "-std=c++0x"
+
+PLATFORM = i486
+eval( $MER_SSH_TARGET_NAME = SailfishOS-3.3.0.16-armv7hl) {
+  message ( "compiling for Phone" )
+  PLATFORM = armv
+}
+
+
+INCLUDEPATH += ../thirdparty/SailfishWidgets/$${PLATFORM}
+
+SOURCES += src/$${TARGET}.cpp
+
+OTHER_FILES += qml/* \
+    qml/cover/*.qml \
+    qml/pages/* \
+    rpm/harbour-fallingblocks.changes.in \
+    rpm/harbour-fallingblocks.spec \
+    rpm/harbour-fallingblocks.yaml \
+    translations/*.ts \
+    ../thirdparty/QmlLogger/* \
+    ../thirdparty/SailfishWidgets/$${PLATFORM}/* \
+    harbour-fallingblocks.desktop
+
+QML_IMPORT_PATH = .
+fallingblocks.files = harbour
+fallingblocks.path = /usr/share/$${TARGET}
+INSTALLS += fallingblocks
+
+swl.files = ../thirdparty/SailfishWidgets/$${PLATFORM}/SailfishWidgets
+swl.path = /usr/share/$${TARGET}/harbour/fallingblocks
+INSTALLS += swl
+
+### Rename QML modules for Harbour store
+swlc.path = /usr/share/$${TARGET}/harbour/fallingblocks
+swlc.commands = find /home/deploy/installroot$$swl.path -name 'qmldir' -exec sed -i \"s/module Sail/module harbour.fallingblocks.Sail/\" \\{} \;;
+swlc.commands += find /home/deploy/installroot$$swl.path -name '*.qmltypes' -exec sed -i \"s/SailfishWidgets/harbour\/fallingblocks\/SailfishWidgets/\" \\{} \;
+INSTALLS += swlc
+
+qmllogger.files = ../thirdparty/QmlLogger/qmldir ../thirdparty/QmlLogger/Logger.js
+qmllogger.path = /usr/share/$${TARGET}/harbour/fallingblocks/QmlLogger
+INSTALLS += qmllogger
+
+#nemosynelibs.files = ../thirdparty/sailfish-widgets/src/qml/SailfishWidgets/Settings/libapplicationsettings* \
+fallingblockslibs.files = $$OUT_PWD/../thirdparty/SailfishWidgets/$${PLATFORM}/SailfishWidgets/Core/libcore.so.1
+fallingblockslibs.path = /usr/share/$${TARGET}/lib
+fallingblockslibs.commands = "pushd /home/deploy/installroot/usr/share/$${TARGET}/lib; ln -s ../harbour/fallingblocks/SailfishWidgets/Settings/libapplicationsettings.so .;  ln -s ../harbour/fallingblocks/SailfishWidgets/Core/libcore.so ."
+INSTALLS += fallingblockslibs
+
+# Linker instructions--The order of -L and -l is important
+LIBS += -L$$_PRO_FILE_PWD_/../thirdparty/SailfishWidgets/$${PLATFORM}/SailfishWidgets/Settings \
+        -lapplicationsettings \
+        -L$$_PRO_FILE_PWD_/../thirdparty/SailfishWidgets/$${PLATFORM}/SailfishWidgets/Core \
+        -lcore
+
+# to disable building translations every time, comment out the
+# following CONFIG line
+CONFIG += sailfishapp_i18n
+TRANSLATIONS += translations/harbour-fallingblocks-de.ts \
+                translations/harbour-fallingblocks-ja.ts
+
+RESOURCES += \
+    images.qrc
+
+DISTFILES +=
+
+
---
+++ fallingblocks/translations/harbour-fallingblocks.ts
@@ -0,0 +1,162 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1">
+<context>
+    <name>CoverPage</name>
+    <message>
+        <source>In Progress</source>
+        <translation></translation>
+    </message>
+    <message>
+        <source>Falling Blocks</source>
+        <translation></translation>
+    </message>
+</context>
+<context>
+    <name>Help</name>
+    <message>
+        <source>How to play</source>
+        <translation></translation>
+    </message>
+    <message>
+        <source>It&apos;s simple! Catch as many blocks as you can while avoiding the mysterious &apos;evil block&apos;.</source>
+        <translation></translation>
+    </message>
+    <message>
+        <source>Move your player by pressing and holding the stationary block at the bottom of the screen. You may move left and right in order to align yourself to the falling block above. Upon successful alignment you will increase your score. However, touching the &apos;evil block&apos; will descrease your score and the number of lives you have. The game ends once all lives are depleted.</source>
+        <translation></translation>
+    </message>
+    <message>
+        <source>Help</source>
+        <translation></translation>
+    </message>
+</context>
+<context>
+    <name>Main</name>
+    <message>
+        <source>Start</source>
+        <translation></translation>
+    </message>
+    <message>
+        <source>Falling blocks</source>
+        <translation>Falling Blocks</translation>
+    </message>
+    <message>
+        <source>Help</source>
+        <translation></translation>
+    </message>
+    <message>
+        <source>New Game</source>
+        <translation></translation>
+    </message>
+    <message>
+        <source>In Progress</source>
+        <translation></translation>
+    </message>
+    <message>
+        <source>Settings</source>
+        <translation></translation>
+    </message>
+</context>
+<context>
+    <name>Score</name>
+    <message>
+        <source>Scoring</source>
+        <translation></translation>
+    </message>
+    <message>
+        <source>Slowest block</source>
+        <translation></translation>
+    </message>
+    <message>
+        <source>Slightly faster</source>
+        <translation></translation>
+    </message>
+    <message>
+        <source>Fastest</source>
+        <translation></translation>
+    </message>
+    <message>
+        <source>Takes a life</source>
+        <translation></translation>
+    </message>
+</context>
+<context>
+    <name>SettingsPage</name>
+    <message>
+        <source>Settings</source>
+        <translation></translation>
+    </message>
+    <message>
+        <source>Starting Lives</source>
+        <translation></translation>
+    </message>
+    <message>
+        <source>3</source>
+        <translation></translation>
+    </message>
+    <message>
+        <source>5</source>
+        <translation></translation>
+    </message>
+    <message>
+        <source>Infinite</source>
+        <translation></translation>
+    </message>
+    <message>
+        <source>Mechanics</source>
+        <translation></translation>
+    </message>
+    <message>
+        <source>Miscellaneous</source>
+        <translation></translation>
+    </message>
+    <message>
+        <source>Disable Page Navigation in Game</source>
+        <translation></translation>
+    </message>
+</context>
+<context>
+    <name>World</name>
+    <message>
+        <source>Score</source>
+        <translation></translation>
+    </message>
+    <message>
+        <source>Lives</source>
+        <translation></translation>
+    </message>
+    <message>
+        <source>Game Over</source>
+        <translation></translation>
+    </message>
+    <message>
+        <source>∞</source>
+        <translation></translation>
+    </message>
+    <message>
+        <source>Easy</source>
+        <translation></translation>
+    </message>
+    <message>
+        <source>Medium</source>
+        <translation></translation>
+    </message>
+    <message>
+        <source>Hard</source>
+        <translation></translation>
+    </message>
+    <message>
+        <source>Difficulty</source>
+        <translation></translation>
+    </message>
+    <message>
+        <source>Extreme</source>
+        <translation></translation>
+    </message>
+    <message>
+        <source>Nightmare</source>
+        <translation></translation>
+    </message>
+</context>
+</TS>
